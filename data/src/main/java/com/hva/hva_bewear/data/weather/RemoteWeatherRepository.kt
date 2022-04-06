2d3f9d0b1cb830bee9774b0e838a1197eff7b6fa
R.C. Wolterbeek <ruben.wolterbeek@hva.nl>
2022-04-06 12:52:04 +0200

    Added WeatherMapper.kt and cleaned up conversion of response models to domain models
    Took 14 minutes

--- data/src/main/java/com/hva/hva_bewear/data/weather/RemoteWeatherRepository.kt
+++ data/src/main/java/com/hva/hva_bewear/data/weather/RemoteWeatherRepository.kt
@@ -1,5 +1,6 @@
 package com.hva.hva_bewear.data.weather
 
+import com.hva.hva_bewear.data.weather.network.WeatherMapper.toDomain
 import com.hva.hva_bewear.data.weather.network.WeatherService
 import com.hva.hva_bewear.data.weather.network.response.DailyWeatherResponse
 import com.hva.hva_bewear.data.weather.network.response.HourlyWeatherResponse
@@ -13,95 +14,6 @@ import com.hva.hva_bewear.domain.weather.model.WeatherDetails
 class RemoteWeatherRepository(private val service: WeatherService) : WeatherRepository {
 
     override suspend fun getWeather(): Weather {
-        return parseWeatherResponse(service.getWeather())
-    }
-
-    private fun parseWeatherResponse(response: WeatherResponse):Weather {
-        return Weather(
-            daily = parseDailyResponse(response.daily),
-            hourly = parseHourlyResponse(response.hourly)
-        )
-    }
-
-    private fun parseDailyResponse(response: List<DailyWeatherResponse>): List<DailyWeather> {
-        val dailyList = arrayListOf<DailyWeather>()
-        response.forEach {
-            val weatherDetails = arrayListOf<WeatherDetails>()
-            it.weather.forEach { details ->
-                weatherDetails.add(
-                    WeatherDetails(details.id, details.main, details.description, details.icon)
-                )
-            }
-            dailyList.add(
-                DailyWeather(
-                    date = it.date,
-                    sunrise = it.sunrise,
-                    sunset = it.sunset,
-                    moonrise = it.moonrise,
-                    moonset = it.moonset,
-                    moonPhase = it.moonPhase,
-                    temperature = DailyWeather.TemperatureDay(
-                        it.temperature.day,
-                        it.temperature.min,
-                        it.temperature.max,
-                        it.temperature.night,
-                        it.temperature.evening,
-                        it.temperature.morning
-                    ),
-                    feelsLike = DailyWeather.FeelsLike(
-                        it.feelsLike.day,
-                        it.feelsLike.night,
-                        it.feelsLike.evening,
-                        it.feelsLike.morning
-                    ),
-                    pressure = it.pressure,
-                    humidity = it.humidity,
-                    dewPoint = it.dewPoint,
-                    windSpeed = it.windSpeed,
-                    windDegree = it.windDegree,
-                    windGust = it.windGust,
-                    weather = weatherDetails,
-                    clouds = it.clouds,
-                    percentageOfPrecipitation = it.percentageOfPrecipitation,
-                    uvIndex = it.uvIndex,
-                    rain = it.rain,
-                    snow = it.snow,
-                )
-            )
-        }
-        return dailyList
-    }
-
-    private fun parseHourlyResponse(response: List<HourlyWeatherResponse>): List<HourlyWeather> {
-        val hourlyWeather = arrayListOf<HourlyWeather>()
-        response.forEach {
-            val weatherDetails = arrayListOf<WeatherDetails>()
-            it.weather.forEach { details ->
-                weatherDetails.add(
-                    WeatherDetails(details.id, details.main, details.description, details.icon)
-                )
-            }
-            hourlyWeather.add(
-                HourlyWeather(
-                    date = it.date,
-                    temperature = it.temperature,
-                    feelsLike = it.feelsLike,
-                    pressure = it.pressure,
-                    humidity = it.humidity,
-                    dewPoint = it.dewPoint,
-                    uvIndex = it.uvIndex,
-                    clouds = it.clouds,
-                    visibility = it.visibility,
-                    windSpeed = it.windSpeed,
-                    windDegree = it.windDegree,
-                    windGust = it.windGust,
-                    weather = weatherDetails,
-                    percentageOfPrecipitation = it.percentageOfPrecipitation,
-                    rain = HourlyWeather.HourlyPrecipitation(it.rain.hour),
-                    snow = HourlyWeather.HourlyPrecipitation(it.snow.hour),
-                )
-            )
-        }
-        return hourlyWeather
+        return service.getWeather().toDomain()
     }
 }
---
+++ data/src/main/java/com/hva/hva_bewear/data/weather/network/WeatherMapper.kt
@@ -0,0 +1,81 @@
+package com.hva.hva_bewear.data.weather.network
+
+import com.hva.hva_bewear.data.weather.network.response.DailyWeatherResponse
+import com.hva.hva_bewear.data.weather.network.response.HourlyWeatherResponse
+import com.hva.hva_bewear.data.weather.network.response.WeatherDetailsResponse
+import com.hva.hva_bewear.data.weather.network.response.WeatherResponse
+import com.hva.hva_bewear.domain.weather.model.DailyWeather
+import com.hva.hva_bewear.domain.weather.model.HourlyWeather
+import com.hva.hva_bewear.domain.weather.model.Weather
+import com.hva.hva_bewear.domain.weather.model.WeatherDetails
+
+object WeatherMapper {
+    fun WeatherResponse.toDomain(): Weather {
+        return Weather(
+            daily = this.daily.map { it.toDomain() },
+            hourly = this.hourly.map { it.toDomain() },
+        )
+    }
+
+    fun DailyWeatherResponse.toDomain(): DailyWeather {
+        return DailyWeather(
+            date = this.date,
+            sunrise = this.sunrise,
+            sunset = this.sunset,
+            moonrise = this.moonrise,
+            moonset = this.moonset,
+            moonPhase = this.moonPhase,
+            temperature = DailyWeather.TemperatureDay(
+                this.temperature.day,
+                this.temperature.min,
+                this.temperature.max,
+                this.temperature.night,
+                this.temperature.evening,
+                this.temperature.morning
+            ),
+            feelsLike = DailyWeather.FeelsLike(
+                this.feelsLike.day,
+                this.feelsLike.night,
+                this.feelsLike.evening,
+                this.feelsLike.morning
+            ),
+            pressure = this.pressure,
+            humidity = this.humidity,
+            dewPoint = this.dewPoint,
+            windSpeed = this.windSpeed,
+            windDegree = this.windDegree,
+            windGust = this.windGust,
+            weather = this.weather.map { it.toDomain() },
+            clouds = this.clouds,
+            percentageOfPrecipitation = this.percentageOfPrecipitation,
+            uvIndex = this.uvIndex,
+            rain = this.rain,
+            snow = this.snow,
+        )
+    }
+
+    fun HourlyWeatherResponse.toDomain(): HourlyWeather {
+        return HourlyWeather(
+            date = this.date,
+            temperature = this.temperature,
+            feelsLike = this.feelsLike,
+            pressure = this.pressure,
+            humidity = this.humidity,
+            dewPoint = this.dewPoint,
+            uvIndex = this.uvIndex,
+            clouds = this.clouds,
+            visibility = this.visibility,
+            windSpeed = this.windSpeed,
+            windDegree = this.windDegree,
+            windGust = this.windGust,
+            weather = this.weather.map { it.toDomain() },
+            percentageOfPrecipitation = this.percentageOfPrecipitation,
+            rain = HourlyWeather.HourlyPrecipitation(this.rain.hour),
+            snow = HourlyWeather.HourlyPrecipitation(this.snow.hour),
+        )
+    }
+
+    fun WeatherDetailsResponse.toDomain():WeatherDetails {
+        return WeatherDetails(this.id, this.main, this.description, this.icon)
+    }
+}
