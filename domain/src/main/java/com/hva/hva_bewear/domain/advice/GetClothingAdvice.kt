1c808feaa505714b1c10112918d54f9533af2bea
Yolojordy <71704175+YoloJordy@users.noreply.github.com>
2022-04-12 10:53:55 +0200

    Moved string concatenation to presentation layer added adviceViewModel

--- domain/src/main/java/com/hva/hva_bewear/domain/advice/GetClothingAdvice.kt
+++ domain/src/main/java/com/hva/hva_bewear/domain/advice/GetClothingAdvice.kt
@@ -29,59 +29,8 @@ class GetClothingAdvice(private val repository: WeatherRepository) {
         advice.rain = currentWeather.percentageOfPrecipitation > 0.3 && currentWeather.rain > 0.5
         advice.highUVI = currentWeather.uvIndex >= 5
 
-        generateTextAdvice()
-
         return advice
     }
 
-    private fun generateTextAdvice(){
-        val extraAdvice: String
-        val extraText: String
-        when{
-            advice.wind && advice.highUVI && advice.rain ->
-            {
-                extraText = ", windy, rainy and sunny"
-                extraAdvice = " It will be rainy, windy and sunny! Take some sunscreen and an umbrella with you but don’t be careful with the wind."
-            }
-            advice.wind && advice.highUVI ->
-            {
-                extraText = ", windy and sunny"
-                extraAdvice = " You should use some sunscreen and put on your hat and sunglasses, but be careful of the wind."
-            }
-            advice.wind && advice.rain ->
-            {
-                extraText = ", windy and rainy"
-                extraAdvice = " Bring an umbrella but don't lose it to the wind!"
-            }
-            advice.highUVI && advice.rain ->
-            {
-                extraText = ", rainy and sunny"
-                extraAdvice = " It will be both rainy and sunny, take some sunscreen and an umbrella with you."
-            }
-            advice.wind ->
-            {
-                extraText = " and windy"
-                extraAdvice = ""
-            }
-            advice.highUVI ->
-            {
-                extraText = " and sunny"
-                extraAdvice = " You should use some sunscreen and put on your hat and sunglasses."
-            }
-            advice.rain ->
-            {
-                extraText = " and rainy"
-                extraAdvice = " Make sure to bring an umbrella."
-            }
-            else ->
-            {
-                extraText = ""
-                extraAdvice = ""
-            }
-        }
 
-        advice.textAdvice = advice.textAdvice.replace("%d", extraText)
-        if (extraText != "") advice.textAdvice = advice.textAdvice.replace("regular", "medium temperature")
-        advice.textAdvice = advice.textAdvice.plus(extraAdvice)
-    }
 }
