25f547ad6f3740ef3aa4427219db1fcc30a0e155
Yolojordy <71704175+YoloJordy@users.noreply.github.com>
2022-04-12 15:26:20 +0200

    Started UI
    Added the card in main screen that displays the current clothing advice in text form

--- app/src/main/java/com/hva/hva_bewear/main/MainActivity.kt
+++ app/src/main/java/com/hva/hva_bewear/main/MainActivity.kt
@@ -3,14 +3,24 @@ package com.hva.hva_bewear.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.hva.hva_bewear.domain.advice.model.ClothingAdvice
 import com.hva.hva_bewear.domain.weather.model.Weather
 import com.hva.hva_bewear.main.theme.M2Mobi_HvATheme
 import com.hva.hva_bewear.presentation.main.MainViewModel
@@ -38,7 +48,12 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun MainScreen() {
         val weather by viewModel.weather.observeAsState()
-        DisplayWeatherData(weather)
+        val advice by viewModel.advice.observeAsState()
+        Column {
+            AdviceDescription(advice)
+            DisplayWeatherData(weather)
+        }
+
     }
 
     @Composable
@@ -46,6 +61,37 @@ class MainActivity : ComponentActivity() {
         Text(
             text = weather.toString()
         )
+    }
+
+    @Composable
+    fun AdviceDescription(advice: ClothingAdvice?){
+        Card(
+            shape = RoundedCornerShape(10.dp),
+            modifier = Modifier.padding(10.dp),
+            elevation = 10.dp,
+        ) {
+            Column {
+                Text(
+                    text = "Clothing Description",
+                    modifier = Modifier
+                        .scale(2f)
+                        .align(Alignment.CenterHorizontally)
+                        .padding(16.dp),
+                )
+                AdviceText(advice = advice)
+            }
+        }
+    }
+
+    @Composable
+    fun AdviceText(advice:ClothingAdvice?){
+        if (advice != null) {
+            Text(
+                text = advice.textAdvice,
+                modifier = Modifier
+                    .padding(16.dp),
+            )
+        }
     }
 
     @Preview(showBackground = true)
