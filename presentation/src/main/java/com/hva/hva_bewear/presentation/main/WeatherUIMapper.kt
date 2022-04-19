d25cc273bdd7824d09d8794e2a3404b733930100
Ruben Wolterbeek <ruben.wolterbeek@hva.nl>
2022-04-19 13:50:02 +0200

    Added the correct calculation for the Beaufort scale
    Took 35 minutes

--- presentation/src/main/java/com/hva/hva_bewear/presentation/main/WeatherUIMapper.kt
+++ presentation/src/main/java/com/hva/hva_bewear/presentation/main/WeatherUIMapper.kt
@@ -2,9 +2,8 @@ package com.hva.hva_bewear.presentation.main
 
 import com.hva.hva_bewear.domain.weather.model.DailyWeather
 import com.hva.hva_bewear.domain.weather.model.Weather
-import com.hva.hva_bewear.domain.weather.model.WeatherDetails
 import com.hva.hva_bewear.presentation.main.model.WeatherUIModel
-import kotlin.math.round
+import kotlin.math.pow
 import kotlin.math.roundToInt
 
 object WeatherUIMapper {
@@ -15,7 +14,7 @@ object WeatherUIMapper {
         return WeatherUIModel(
             temperatureDisplay = parseTemperature(temperature.day),
             feelsLikeTemperatureDisplay = "Feels like: \n${parseTemperature(feelsLike.day)}",
-            windDisplay = "${setWindDirection(windDegree)} ${windSpeed.roundToInt()}",
+            windDisplay = "${setWindDirection(windDegree)} ${calculateBeaufortScale(windSpeed)}",
             iconUrl = "https://openweathermap.org/img/wn/${weather[0].icon}@4x.png",
             windDegrees = windDegree,
         )
@@ -34,5 +33,13 @@ object WeatherUIMapper {
 
             else -> "N"
         }
+    }
+
+    /**
+     * Uses the general formula used to calculate the beaufort scale, v = 0.836B^(3/2) (v = m/s and B = beaufort scale)
+     * This formula is converted to take the form of B = ...v^... so in this case B = (v/0.836)^(2/3)
+     */
+    private fun calculateBeaufortScale(windSpeed:Double):Int {
+        return ((windSpeed / 0.836).pow(2.0 / 3.0)).roundToInt()
     }
 }
