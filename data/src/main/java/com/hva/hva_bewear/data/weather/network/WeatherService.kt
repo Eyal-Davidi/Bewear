8ee293f6ddc807a52d70586eb4d8bdf5ec396fb0
Ruben Wolterbeek <ruben.wolterbeek@hva.nl>
2022-04-22 16:13:04 +0200

    Added isBeforeHour function for later use
    Took 7 minutes

--- data/src/main/java/com/hva/hva_bewear/data/weather/network/WeatherService.kt
+++ data/src/main/java/com/hva/hva_bewear/data/weather/network/WeatherService.kt
@@ -18,6 +18,7 @@ import java.io.FileNotFoundException
 import java.io.PrintWriter
 import java.time.Instant
 import java.time.LocalDate
+import java.time.LocalDateTime
 import java.time.ZoneId
 
 class WeatherService {
@@ -43,13 +44,14 @@ class WeatherService {
 //        return client.get(url.replace(lat, location.lat.toString()).replace(lon, location.lon.toString()))
 
         // Use locally stored files to cache the api data
-        var file = File(context.filesDir, "${location.cityName.lowercase()}.json")
+        val fileName = "${location.cityName.lowercase().replace(" ", "")}.json"
+        var file = File(context.filesDir, fileName)
         // If the file does not yet exists a new file is created
-        if(!file.exists()) file = createNewFile(file)
+        if (!file.exists()) file = createNewFile(file)
 
         var weather: WeatherResponse = json.decodeFromString(file.readText())
         // If the date in the file is before the current date the file is refreshed
-        if(instantToDate(weather.daily[0].date).isBefore(LocalDate.now())) {
+        if (dateIsBeforeCurrentDay(weather.daily[0].date)) {
             writeApiDataToFile(file)
             weather = json.decodeFromString(file.readText())
         }
@@ -61,7 +63,7 @@ class WeatherService {
             file.createNewFile()
             writeApiDataToFile(file)
         }
-        if(!file.exists()) throw FileNotFoundException()
+        if (!file.exists()) throw FileNotFoundException()
         return file
     }
 
@@ -78,15 +80,39 @@ class WeatherService {
         return file
     }
 
-    private fun instantToDate(instant: Int): LocalDate {
-        return Instant.ofEpochSecond(instant.toLong())
+    /**
+     * Calculates if the provided dateInt is before the current hour
+     */
+    private fun dateIsBeforeCurrentHour(dateInt: Int): Boolean {
+        val dateTime = dateInt.instantToDateTime()
+        val now = LocalDateTime.now()
+        return dateIsBeforeCurrentDay(dateInt) ||
+                dateTime.hour < now.hour
+    }
+
+    /**
+     * Calculates if the provided dateInt is before the current day
+     */
+    private fun dateIsBeforeCurrentDay(dateInt: Int): Boolean {
+        val date = dateInt.instantToDate()
+        return date.isBefore(LocalDate.now())
+    }
+
+    private fun Int.instantToDate(): LocalDate {
+        return Instant.ofEpochSecond(toLong())
             .atZone(ZoneId.systemDefault())
             .toLocalDate()
     }
 
+    private fun Int.instantToDateTime(): LocalDateTime {
+        return Instant.ofEpochSecond(toLong())
+            .atZone(ZoneId.systemDefault())
+            .toLocalDateTime()
+    }
+
     companion object {
-        private const val lat ="100000"
-        private const val lon ="20000"
+        private const val lat = "100000"
+        private const val lon = "20000"
         private const val url =
             "https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,current&units=metric&appid=***REMOVED***"
     }
