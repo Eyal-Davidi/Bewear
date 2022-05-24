be3d78ff8a935ba54de3a35a4c146cea6f5c5679
Yolojordy <71704175+YoloJordy@users.noreply.github.com>
2022-05-24 10:26:30 +0200

    Added the nunito font

--- app/src/main/java/com/hva/bewear/main/theme/Type.kt
+++ app/src/main/java/com/hva/bewear/main/theme/Type.kt
@@ -2,14 +2,36 @@ package com.hva.bewear.main.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.hva.bewear.R
+
+val nunito = FontFamily(
+    //Font(R.font.nunito_black, weight = FontWeight.Normal, style = FontStyle.Normal),
+    //Font(R.font.nunito_black_italic),
+    Font(R.font.nunito_bold, weight = FontWeight.Normal, style = FontStyle.Normal),
+    Font(R.font.nunito_bold_italic, weight = FontWeight.Normal, style = FontStyle.Italic),
+    Font(R.font.nunito_extra_bold, weight = FontWeight.Bold, style = FontStyle.Normal),
+    Font(R.font.nunito_extra_bold_italic, weight = FontWeight.Bold, style = FontStyle.Italic),
+    //Font(R.font.nunito_extra_light),
+    //Font(R.font.nunito_extra_light_italic),
+    //Font(R.font.nunito_italic),
+    //Font(R.font.nunito_light),
+    //Font(R.font.nunito_light_italic),
+    //Font(R.font.nunito_medium),
+    //Font(R.font.nunito_medium_italic),
+    //Font(R.font.nunito_regular),
+    Font(R.font.nunito_semi_bold, weight = FontWeight.Light, style = FontStyle.Normal),
+    Font(R.font.nunito_semi_bold_italic, weight = FontWeight.Light, style = FontStyle.Italic),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = nunito,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
