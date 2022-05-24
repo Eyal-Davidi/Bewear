bc6f306f4dd8b35876f48d95df82daa88684fbef
Yolojordy <71704175+YoloJordy@users.noreply.github.com>
2022-05-24 11:07:29 +0200

    fix import

--- app/src/main/java/com/hva/bewear/main/theme/Type.kt
+++ app/src/main/java/com/hva/bewear/main/theme/Type.kt
@@ -7,7 +7,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import com.hva.bewear.R
+import com.hva_bewear.R
 
 val nunito = FontFamily(
     //Font(R.font.nunito_black, weight = FontWeight.Normal, style = FontStyle.Normal),
